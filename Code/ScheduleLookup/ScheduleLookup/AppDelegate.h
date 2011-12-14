bb4d4bbed437730fd58fd330e5b1e2561280726c
Mark Vitale <vitalema@rose-hulman.edu>
2011-12-14 12:28:09 -0500

    Created the basic Tab Bar Controller template project.
    All code is template code, no changes have been made.

---
+++ Code/ScheduleLookup/ScheduleLookup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 12/14/11.
+//  Copyright (c) 2011 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Code/ScheduleLookup/ScheduleLookup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 12/14/11.
+//  Copyright (c) 2011 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
