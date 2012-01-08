8722d2d4c6e9690f34a829c770b3d3abaa3eb3a1
Mark Vitale <vitalema@rose-hulman.edu>
2012-01-08 12:40:17 -0500

    Create a kerberos test project that does authentication with the Schedule Lookup Page.

---
+++ Code/Kerberos Test/Kerberos Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Kerberos Test
+//
+//  Created by Mark Vitale on 1/8/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Code/Kerberos Test/Kerberos TestTests/Kerberos_TestTests.h
@@ -0,0 +1,13 @@
+//
+//  Kerberos_TestTests.h
+//  Kerberos TestTests
+//
+//  Created by Mark Vitale on 1/8/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Kerberos_TestTests : SenTestCase
+
+@end
