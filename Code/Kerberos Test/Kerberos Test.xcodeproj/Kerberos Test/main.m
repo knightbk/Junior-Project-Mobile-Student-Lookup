b5937fe607ce2ebed2d52ad6c1e2b313e82e5bf9
Brandon knight <knightbk@rose-hulman.edu>
2012-01-13 14:08:32 -0500

    git is being weird

---
+++ Code/Kerberos Test/Kerberos Test.xcodeproj/Kerberos Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kerberos Test
+//
+//  Created by Mark Vitale on 1/8/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
