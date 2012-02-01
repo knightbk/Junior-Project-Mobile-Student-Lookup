43bb7185357dfa5d77b28e2e4d65616311dcd777
Brandon knight <knightbk@rose-hulman.edu>
2012-02-01 15:50:09 -0500

    factory
    added some factory stuff, committing before I pack up, just in case.

---
+++ Code/ScheduleLookup/ScheduleLookup/StudentFactory.m
@@ -0,0 +1,36 @@
+//
+//  Factory.m
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 1/26/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+
+#import "StudentFactory.h"
+#import "Factory.h"
+
+@implementation StudentFactory
+
++ (Student *) studentFromStudentSchedulePage:(NSString *) html
+{
+    return [[Student alloc] initStudentWithAlias:[self usernameFromStringData:html] 
+                                    WithCmNumber:nil 
+                                        WithName:[super nameFromStringData:html] 
+                                       WithMajor:nil 
+                                        WithYear:nil 
+                                     WithAdvisor:[self advisorFromStringData:html]];
+}
+
+
+
++ (NSString *) advisorFromStringData:(NSString *)sdata
+{
+    return [self firstMatchStringWithRegex:@"> Advisor: ([a-zA-Z ]+)<" WithStringData:sdata];
+}
+
+
+
+
+
+@end
