c5f3f7333ef8c367b32868402ffa057af5dacaf6
Brandon knight <knightbk@rose-hulman.edu>
2012-02-01 17:27:46 -0500

    Keychain Access
    I got keychain access working in the Kerberos Test App, check it out.

--- Code/Kerberos Test/Kerberos Test/ViewController.h
+++ Code/Kerberos Test/Kerberos Test/ViewController.h
@@ -7,11 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
-
-
+#import "KeychainItemWrapper.h"
+KeychainItemWrapper *keychain;
 @interface ViewController : UIViewController
-- (IBAction)someMethod:(id)sender;
+ - (IBAction) someMethod:(id)sender;
+- (IBAction) savePassword:(id)sender;
+- (IBAction)recoverPassword:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *testButton;
+@property (weak, nonatomic) IBOutlet UIButton *recoverButton;
 
 @end
