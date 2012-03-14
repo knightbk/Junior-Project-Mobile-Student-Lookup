2b4770b2e2d9a8a2bd488fdb7069c694391a0ab8
Nick Crawford <crawfonw@rose-hulman.edu>
2012-03-14 12:19:00 -0400

    Fix email bug and clean up code

--- Code/ScheduleLookup/ScheduleLookup/UserInfoViewController.m
+++ Code/ScheduleLookup/ScheduleLookup/UserInfoViewController.m
@@ -28,19 +28,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void) viewWillAppear:(BOOL)animated
@@ -101,13 +93,10 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    // Configure the cell...
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
-    
-    // Configure the cell...
     if (indexPath.section == 0) {
         
         cell.textLabel.text = [infoList objectAtIndex:indexPath.row];
@@ -121,25 +110,6 @@
             cell.userInteractionEnabled = NO;
             
         }
-        /*switch (indexPath.row) {
-            case 0:
-                cell.textLabel.text = [self.person getEmailAddress];
-                break;
-            case 1:
-                cell.textLabel.text = [self.person phoneNumber];
-                break;
-            case 2:
-                cell.textLabel.text = [self.person cmNumber];
-                break;
-            case 3:
-                cell.textLabel.text = [self.person officeNumber];
-                break;
-            case 4:
-                cell.textLabel.text = [self.person department];
-                break;
-            default:
-                break;
-        }*/
     } else {
         switch (indexPath.row) {
             case 0:
@@ -180,82 +150,33 @@
     return UITableViewCellAccessoryNone;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */     
-   
-            if(indexPath.section == 0)
-            {
-                if(indexPath.row == 0)
-                {
-                    NSLog(@"Send E-mail to %@", person.getEmailAddress);
-                    
-                    NSString * email = [NSString stringWithFormat:@"email://%@",person.getPhoneNumberWithAreaCode];
-                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:email]];
-                    
-                }
-                else if(indexPath.row == 1 && indexPath.section == 0 && ![[self.person phoneNumber] isEqualToString:@""])
-                {
-                    NSLog(@"Calling %@", person.getPhoneNumberWithAreaCode);
-                    
-                    NSString * phone = [NSString stringWithFormat:@"tel://%@",person.getPhoneNumberWithAreaCode];
-                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phone]];
-                }
-                
-            }
-            else
-            {
-                
-            }
-
+    if(indexPath.section == 0)
+    {
+        if(indexPath.row == 0)
+        {
+            NSLog(@"Send E-mail to %@", person.getEmailAddress);
+            
+            NSString * email = [NSString stringWithFormat:@"mailto:%@",person.getEmailAddress];
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:email]];
+            
+        }
+        else if(indexPath.row == 1 && indexPath.section == 0 && ![[self.person phoneNumber] isEqualToString:@""])
+        {
+            NSLog(@"Calling %@", person.getPhoneNumberWithAreaCode);
+            
+            NSString * phone = [NSString stringWithFormat:@"tel://%@",person.getPhoneNumberWithAreaCode];
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phone]];
+        }
         
+    }
+    else
+    {
+        
+    }
 }
 
 @end
