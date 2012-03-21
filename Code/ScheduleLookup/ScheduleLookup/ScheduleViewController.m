398cd2b791fce236f29b2acff51be21dbb44322d
Mark Vitale <vitalema@rose-hulman.edu>
2012-03-20 22:46:06 -0400

    Puts in some stuff for displaying schedules.
    Need to extend UITableView, just using them now to show sample
    functionality.

--- Code/ScheduleLookup/ScheduleLookup/ScheduleViewController.m
+++ Code/ScheduleLookup/ScheduleLookup/ScheduleViewController.m
@@ -29,20 +29,11 @@
 
 #pragma mark - View lifecycle
 
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView
-{
-}
-*/
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
 }
-*/
 
 - (void)viewDidUnload
 {
