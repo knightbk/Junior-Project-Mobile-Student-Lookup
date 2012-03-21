cf3dcafb39378cc07f9ce2e43d4938ad64b8d86c
Mark Vitale <vitalema@rose-hulman.edu>
2012-03-20 20:48:51 -0400

    Add a Schedule View Controller class.

---
+++ Code/ScheduleLookup/ScheduleLookup/ScheduleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleViewController.h
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 3/20/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleViewController : UIViewController
+
+@end
