fdadab09c6eeb88bea4b04d782a724413c5ada6a
Mark Vitale <vitalema@rose-hulman.edu>
2012-04-12 19:13:02 -0400

    Add RosterFactory to create a dictionary of username to name entries for a class.  Just blank files at this point.

---
+++ Code/ScheduleLookup/ScheduleLookup/RosterFactory.h
@@ -0,0 +1,13 @@
+//
+//  RosterFactory.h
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 4/12/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RosterFactory : NSObject
+
+@end
---
+++ Code/ScheduleLookup/ScheduleLookup/RosterFactory.m
@@ -0,0 +1,13 @@
+//
+//  RosterFactory.m
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 4/12/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import "RosterFactory.h"
+
+@implementation RosterFactory
+
+@end
