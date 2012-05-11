5381d734b3860953b31a24418c72a889f3496b69
Mark Vitale <vitalema@rose-hulman.edu>
2012-05-10 20:05:47 -0400

    Add Integration Test File

---
+++ Code/ScheduleLookup/ScheduleLookupTests/IntegrationTests.h
@@ -0,0 +1,13 @@
+//
+//  IntegrationTests.h
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 5/10/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IntegrationTests : SenTestCase
+
+@end
---
+++ Code/ScheduleLookup/ScheduleLookupTests/IntegrationTests.m
@@ -0,0 +1,13 @@
+//
+//  IntegrationTests.m
+//  ScheduleLookup
+//
+//  Created by Mark Vitale on 5/10/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import "IntegrationTests.h"
+
+@implementation IntegrationTests
+
+@end
